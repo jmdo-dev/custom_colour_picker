aeba6faf185147af79131fc709fa757cfa8a4dbb
Joe OMalley <joemdomalley@gmail.com>
2019-09-18 11:47:57 +0100

    Start styling mini palette with JSS

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import MiniPalette from './MiniPalette';
 
 
  class PaletteList extends Component {
@@ -9,12 +10,7 @@ import { Link } from 'react-router-dom'
       <div>
         <h1>React Colors</h1>
         {palettes.map(palette => (
-          <p>
-            <Link
-              to={`/palette/${palette.id}`} >
-              {palette.paletteName} 
-            </Link>
-          </p>
+          <MiniPalette {...palette} />
         ))}
       </div>
     )
