4bf246407a158248760416d64e8ac429e49807ea
Joe OMalley <joemdomalley@gmail.com>
2019-09-19 11:23:51 +0100

    Connect color picker to button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -74,9 +74,16 @@ const styles = theme => ({
 });
 
 class NewPaletteForm extends Component {
-  state = {
-    open: false,
-  };
+  constructor(props) {
+    super(props)
+    this.state = { 
+      open: true, 
+      currentColor: 'teal',
+      colors: ['purple', '#e15675']
+     }
+    this.updateCurrentColor = this.updateCurrentColor.bind(this)
+    this.addNewColor = this.addNewColor.bind(this)
+  }
 
   handleDrawerOpen = () => {
     this.setState({ open: true });
@@ -85,6 +92,14 @@ class NewPaletteForm extends Component {
   handleDrawerClose = () => {
     this.setState({ open: false });
   };
+
+  updateCurrentColor(newColor){
+    this.setState({ currentColor: newColor.hex})
+  }
+
+  addNewColor(){
+    this.setState({colors: [...this.state.colors, this.state.currentColor]})
+  }
 
   render() {
     const { classes } = this.props;
@@ -133,10 +148,17 @@ class NewPaletteForm extends Component {
             <Button variant='contained' color='primary'>Random Color</Button>
           </div>
           <ChromePicker
-           color='purple'
-           onChangeComplete={newColor => console.log(newColor)}
+           color={this.state.currentColor}
+           onChangeComplete={this.updateCurrentColor}
           />
-          <Button variant='contained' color='primary'>Add Color</Button>
+          <Button 
+            variant='contained' 
+            color='primary' 
+            style= {{backgroundColor: this.state.currentColor}}
+            onClick={this.addNewColor}
+          > 
+          Add Color
+          </Button>
           
         </Drawer>
         <main
@@ -145,7 +167,11 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-         
+          <ul>
+            {this.state.colors.map(color => (
+              <li style={{backgroundColor: color}}>{color}</li>
+            ))}
+          </ul>
         </main>
       </div>
     );
